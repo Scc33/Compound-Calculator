a1c9bba2b869ac8d10732afb48ce8af914ca9bd6
Sean Coughlin <seanmc4@illinois.edu>
2021-06-26 23:21:40 -0600

    Start some stack work

---
+++ SwiftPractice/ArrayList.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+struct IntArrayList {
+    var size: Int
+    var maxSize: Int
+    var array: [Int]
+    
+    func removeFront() {
+        
+    }
+    
+    func removeBack() {
+        
+    }
+    
+    func addElement(index: Int, element: Int) {
+        
+    }
+    
+    func removeElement(index: Int) {
+        
+    }
+}
---
+++ SwiftPractice/Stack.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+struct IntStack {
+    var size: Int
+    var array: [Int]
+    
+    func peek() {
+        
+    }
+    
+    func pop() {
+        
+    }
+    
+    func add() {
+        
+    }
+}
