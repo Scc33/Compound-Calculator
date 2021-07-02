d5faf5a7a5bfc0e72833c990f241e173eea51311
Sean Coughlin <seanmc4@illinois.edu>
2021-07-01 18:54:31 -0600

    Adjust Swift practice file locations and start pointer practice

---
+++ SwiftPractice/DataStructures/Pointers.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+MemoryLayout<Int>.size          // returns 8 (on 64-bit)
+MemoryLayout<Int>.alignment     // returns 8 (on 64-bit)
+MemoryLayout<Int>.stride        // returns 8 (on 64-bit)
+
+MemoryLayout<Int16>.size        // returns 2
+MemoryLayout<Int16>.alignment   // returns 2
+MemoryLayout<Int16>.stride      // returns 2
+
+MemoryLayout<Bool>.size         // returns 1
+MemoryLayout<Bool>.alignment    // returns 1
+MemoryLayout<Bool>.stride       // returns 1
+
+MemoryLayout<Float>.size        // returns 4
+MemoryLayout<Float>.alignment   // returns 4
+MemoryLayout<Float>.stride      // returns 4
+
+MemoryLayout<Double>.size       // returns 8
+MemoryLayout<Double>.alignment  // returns 8
+MemoryLayout<Double>.stride     // returns 8
