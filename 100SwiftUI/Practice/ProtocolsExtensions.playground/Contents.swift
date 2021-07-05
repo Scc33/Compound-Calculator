2ef5ce11e66556869957b4721191ca2c5f04c77e
Sean Coughlin <seanmc4@illinois.edu>
2021-07-05 18:44:50 -0500

    Start learning about protocols

--- 100SwiftUI/Practice/ProtocolsExtensions.playground/Contents.swift
+++ 100SwiftUI/Practice/ProtocolsExtensions.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//"Protocols are a way of describing what properties and methods something must have"
+//protocols let us create blueprints of how our types share functionality
+
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+var s = User(id: "1234")
+displayID(thing: s)
+
+//protocols determine the minimum required functionality, but we can always add more
