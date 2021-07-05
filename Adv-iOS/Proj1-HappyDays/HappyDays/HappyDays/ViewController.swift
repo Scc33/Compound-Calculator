f7beeaaf91137287faea4e694805d6836460ba68
Sean Coughlin <seanmc4@illinois.edu>
2021-07-05 14:37:06 -0500

    Create first advanced project HappyDays

---
+++ Adv-iOS/Proj1-HappyDays/HappyDays/HappyDays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyDays
+//
+//  Created by Sean Coughlin on 7/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
