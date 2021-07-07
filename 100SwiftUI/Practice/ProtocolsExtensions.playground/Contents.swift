41fb871da45ae0b10b7e4cfcbc6fad1ae77e2eaa
Sean Coughlin <seanmc4@illinois.edu>
2021-07-06 22:42:21 -0500

    Finish learning about protocols

--- 100SwiftUI/Practice/ProtocolsExtensions.playground/Contents.swift
+++ 100SwiftUI/Practice/ProtocolsExtensions.playground/Contents.swift
@@ -21,3 +21,50 @@ var s = User(id: "1234")
 displayID(thing: s)
 
 //protocols determine the minimum required functionality, but we can always add more
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+//Extensions allow for adding properties to types
+//Has to be a computed property, cannot be stored
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 8
+number.squared()
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+//Arrays in Swift conform to collection protocol, protocols can be expanded using collections
+//"protocol-oriented programming language"
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+//POP is a new concept than OOP
