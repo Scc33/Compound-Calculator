4c43f37fe002866261b4a4f7089267bc726a3778
Sean Coughlin <seanmc4@illinois.edu>
2021-07-11 23:51:16 -0500

    Finish section two review

--- 100SwiftUI/Practice/ReviewTwo.playground/Contents.swift
+++ 100SwiftUI/Practice/ReviewTwo.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+// -> String means we will definitely return a string
+// -> String? means we might or might not return something
+//If let is used to unwrap a variable and check to see if there is something there
+//! is used to for Swift to use a value
+
+//?? - nil coalescin operator
+
+//Enum - allows you to define a new datatype and say what type of data it can contain
+enum WeatherType {
+    case sun, cloud, rain, wind, snow
+}
+
+func getHaterStatus(weather: WeatherType) -> String? {
+    if weather == WeatherType.sun {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+getHaterStatus(weather: WeatherType.cloud)
+
+
+//Classes are used extensively in Cocoa touch
+
+//Structs only use values (pass by value)
+//Classes use references
