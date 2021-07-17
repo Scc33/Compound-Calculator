c3b2a6d624c8ec075e9be86b36feb1632fb3b2ba
Sean Coughlin <seanmc4@illinois.edu>
2021-07-16 23:28:22 -0500

    Start first SwiftUI100 challenge project

---
+++ 100SwiftUI/Challenge1-UnitConvert/UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Sean Coughlin on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
