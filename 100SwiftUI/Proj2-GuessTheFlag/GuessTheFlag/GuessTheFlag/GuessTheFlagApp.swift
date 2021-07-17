9a865c1c6d2ef51ce8189bd47ce29abaeefd04eb
Sean Coughlin <seanmc4@illinois.edu>
2021-07-16 23:59:33 -0500

    Add setup for next SwiftUI project

---
+++ 100SwiftUI/Proj2-GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sean Coughlin on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
