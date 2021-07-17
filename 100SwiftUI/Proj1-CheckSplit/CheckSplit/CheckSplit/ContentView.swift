eb27c490350d4b9a710ccd9019ae11c255a89da0
Sean Coughlin <seanmc4@illinois.edu>
2021-07-16 23:26:01 -0500

    Complete challenges for project 1

--- 100SwiftUI/Proj1-CheckSplit/CheckSplit/CheckSplit/ContentView.swift
+++ 100SwiftUI/Proj1-CheckSplit/CheckSplit/CheckSplit/ContentView.swift
@@ -11,21 +11,32 @@ struct ContentView: View {
     //SwiftUI must use strings to store text field values
     //One of the great things about the @State property wrapper is that it automatically watches for changes, and when something happens it will automatically re-invoke the body property
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var basePerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 0
+        let orderAmount = Double(checkAmount) ?? 0 // need to remove the optional
+        
+        let amountPerPerson = orderAmount / peopleCount
+
+        return amountPerPerson
+    }
+    
+    var tipPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 0
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0 // need to remove the optional
         
         let tipValue = orderAmount / 100 * tipSelection
-        let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+        let amountPerPerson = tipValue / peopleCount
 
-        /*Hopefully you’re now seeing for yourself what it means that SwiftUI’s views are a function of their state – when the state changes, the views automatically update to match.*/
         return amountPerPerson
+    }
+    
+    var totalPerPerson: Double {
+        return basePerPerson + tipPerPerson
     }
     
     var body: some View {
@@ -34,21 +45,30 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    Picker("Number of people", selection: $numberOfPeople) {
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                    /*Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2 ..< 100) {
                             Text("\($0) people")
                         }
-                    }
+                    }*/
                 }
                 Section(header: Text("How much tip do you want to leave?")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
+                            //remember $ is shorthand for closures
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
-                Section {
+                Section(header: Text("Base Cost")) {
+                    Text("$\(basePerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Tip")) {
+                    Text("$\(tipPerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Total cost per person")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
