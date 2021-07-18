511a3d0b220c794d75ec63d44e0a53470cf47fcb
Sean Coughlin <seanmc4@illinois.edu>
2021-07-17 22:39:02 -0500

    Practice for project two

---
+++ 100SwiftUI/Proj2-GuessTheFlag/GuessFlagPractice/GuessFlagPractice/GuessFlagPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagPracticeApp.swift
+//  GuessFlagPractice
+//
+//  Created by Sean Coughlin on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
