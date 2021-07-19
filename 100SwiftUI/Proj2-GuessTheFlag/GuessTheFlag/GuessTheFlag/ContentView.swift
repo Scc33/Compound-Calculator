2cccb1a2963086e7e698ebd99a59f816f1ba2a0c
Sean Coughlin <seanmc4@illinois.edu>
2021-07-18 23:48:42 -0500

    Complete most of project two

--- 100SwiftUI/Proj2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100SwiftUI/Proj2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,30 +10,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
     
     var body: some View {
         ZStack {
             //edgesIgnoringSafeArea allows blue to fill whole screen
-            Color.blue.edgesIgnoringSafeArea(.all)
+            //Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
                     Text(countries[correctAnswer]) .foregroundColor(.white)
+                        .font(.largeTitle) //automatically scales up or down depending on what setting the user has for their fonts – a feature known as Dynamic Type
+                        .fontWeight(.black)
                 }
                 ForEach(0 ..< 3) { number in
                     Button(action: {
                         // flag was tapped
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
                         //modifier tells SwiftUI to render the original image pixels rather than trying to recolor them as a button
                     }
                 }
                 Spacer()
             }
+        }.alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
         }
     }
 }
