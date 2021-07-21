84292e01f9d8733dda22e761e0780a01dfb16712
Sean Coughlin <seanmc4@illinois.edu>
2021-07-20 23:37:55 -0500

    Create project files for views and modifiers wrapup

---
+++ 100SwiftUI/Proj3-ViewsMods/ViewsModifiersWrapup/ViewsModifiersWrapup/ViewsModifiersWrapupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsModifiersWrapupApp.swift
+//  ViewsModifiersWrapup
+//
+//  Created by Sean Coughlin on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsModifiersWrapupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
