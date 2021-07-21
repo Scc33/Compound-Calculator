4c203638a12d08cfdff4fd9ff2b8f1712df54420
Sean Coughlin <seanmc4@illinois.edu>
2021-07-20 23:50:23 -0500

    Create project files for challenge two

---
+++ 100SwiftUI/Challenge2-RockPaperScissors/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Sean Coughlin on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
