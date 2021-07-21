24c6debcce16788b85cf841e8a14fef5341e2dc1
Sean Coughlin <seanmc4@illinois.edu>
2021-07-20 23:47:56 -0500

    Create files for project 4

---
+++ 100SwiftUI/Proj4-BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sean Coughlin on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
