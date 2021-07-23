77134213c935e743782dcabb97b389a0ea5907d7
Sean Coughlin <seanmc4@illinois.edu>
2021-07-22 22:35:44 -0500

    Add BetterRestPrep project

---
+++ 100SwiftUI/Proj4-BetterRest/BetterRestPrep/BetterRestPrep/BetterRestPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestPrepApp.swift
+//  BetterRestPrep
+//
+//  Created by Sean Coughlin on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100SwiftUI/Proj4-BetterRest/BetterRestPrep/BetterRestPrep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRestPrep
+//
+//  Created by Sean Coughlin on 7/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
