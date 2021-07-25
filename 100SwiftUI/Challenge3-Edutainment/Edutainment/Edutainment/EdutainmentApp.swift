54de9d71aea2790f5b5ae4c33224a1add88844a7
Sean Coughlin <seanmc4@illinois.edu>
2021-07-25 12:16:42 -0500

    Create some new projects

---
+++ 100SwiftUI/Challenge3-Edutainment/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Sean Coughlin on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100SwiftUI/Proj6-Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Sean Coughlin on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
