662a37839e306befea337359c6fc2e64750fe7ea
Sean Coughlin <seanmc4@illinois.edu>
2021-07-27 23:17:49 -0500

    Create project seven
    Project seven is where things start to get more complex

---
+++ 100SwiftUI/Proj7-Expense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sean Coughlin on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
