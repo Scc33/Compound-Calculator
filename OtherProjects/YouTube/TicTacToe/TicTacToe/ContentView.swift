258dbca1058ebaf613ffffe4a0d8e2e01304581c
Sean Coughlin <seanmc4@illinois.edu>
2021-07-28 23:32:20 -0500

    Create TicTacToe files
    From Sean Allen tutorial - https://youtu.be/MCLiPW2ns2w

---
+++ OtherProjects/YouTube/TicTacToe/TicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Sean Coughlin on 7/28/21.
+//
+
+// Sean Allen Tutorial
+// https://youtu.be/MCLiPW2ns2w
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OtherProjects/YouTube/TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Sean Coughlin on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
