46f728db66fcf1afac361a9d5cd79c71e18492a6
Sean Coughlin <seanmc4@illinois.edu>
2021-07-29 23:07:07 -0500

    Research two new cool projects off youtube

---
+++ OtherProjects/YouTube/Courses/Courses/Courses/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Courses
+//
+//  Created by Sean Coughlin on 7/29/21.
+//
+
+//https://www.youtube.com/watch?v=1AXyC24NCkE
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OtherProjects/YouTube/Courses/Courses/Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by Sean Coughlin on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
