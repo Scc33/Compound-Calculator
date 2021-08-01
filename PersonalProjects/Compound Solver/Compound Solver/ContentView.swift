6f6697d67df0b230be15ad47aa6e8b4f80451fb3
Sean Coughlin <seanmc4@illinois.edu>
2021-08-01 16:05:30 -0500

    Update with old compound code

--- PersonalProjects/Compound Solver/Compound Solver/ContentView.swift
+++ PersonalProjects/Compound Solver/Compound Solver/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 1
+    @State private var isShareSheetShowing = false
+    
+    func shareButton() {
+        isShareSheetShowing.toggle()
+        let url = URL(string: "https://apple.com")
+        let activityView = UIActivityViewController(activityItems: [url!], applicationActivities: nil)
+        UIApplication.shared.windows.first?.rootViewController?.present(activityView, animated: true, completion: nil)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TabView(selection: $selectedTab) {
+                NavigationView {
+                    ScrollView {
+                        VStack(alignment: .center) {
+                            HStack {
+                                Button(action: shareButton) {
+                                    Image(systemName:  "square.and.arrow.up").font(.largeTitle)
+                                }
+                                Spacer()
+                                Text("List").font(.largeTitle)
+                                Spacer()
+                                Image(systemName:  "gearshape").font(.largeTitle)
+                            }.padding()
+                            HStack {
+                                Text("test")
+                            }
+                            HStack {
+                                Text("test1")
+                            }
+                            HStack {
+                                NavigationView {
+                                    List {
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .navigationTitle(Text("Title"))
+                    .tabItem {
+                        Text("Graph")
+                        Image(systemName:"waveform.path.ecg.rectangle")
+                    }.tag(2)
+                }
+            }
+        }
     }
 }
 
