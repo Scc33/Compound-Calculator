b18ae70469394e5db3a028ccf68f571af4caaebf
Sean Coughlin <seanmc4@illinois.edu>
2021-08-01 16:00:54 -0500

    Add first cut learnings

---
+++ PersonalProjects/First Cut Learning/20 hours beta/20 hours beta/_0_hours_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_hours_betaApp.swift
+//  20 hours beta
+//
+//  Created by Sean Coughlin on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct _0_hours_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
