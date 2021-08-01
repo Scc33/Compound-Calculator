f619c797cbbdeee6aed62720311a9157ef80ea23
Sean Coughlin <seanmc4@illinois.edu>
2021-08-01 16:39:09 -0500

    Work on compound by create second view for rule of 72

--- PersonalProjects/Compound Solver/Compound Solver/ContentView.swift
+++ PersonalProjects/Compound Solver/Compound Solver/ContentView.swift
@@ -13,12 +13,22 @@ struct ContentView: View {
     
     @State private var initial = ""
     @State private var contributions = ""
+    @State private var time = ""
+    
+    @State private var estBase: topLine = .seventy
+    @State private var estInterest = ""
     
     func shareButton() {
         isShareSheetShowing.toggle()
         let url = URL(string: "https://apple.com")
         let activityView = UIActivityViewController(activityItems: [url!], applicationActivities: nil)
         UIApplication.shared.windows.first?.rootViewController?.present(activityView, animated: true, completion: nil)
+    }
+    
+    var estDouble: Double {
+        let convertedEstBase = Double(estBase.id) ?? 0.1
+        let convertedEstInterest = Double(estInterest) ?? 0.1
+        return convertedEstBase / convertedEstInterest
     }
     
     var body: some View {
@@ -34,28 +44,56 @@ struct ContentView: View {
                             }
                             Image(systemName:  "gearshape")
                                 .font(.largeTitle)
-                        }.padding()
+                        }
                         Form {
-                            TextField("Initial", text: $initial)
-                                .keyboardType(.decimalPad)
-                            TextField("Contributions", text: $contributions)
-                                .keyboardType(.decimalPad)
+                            Section {
+                                TextField("Initial", text: $initial)
+                                    .keyboardType(.decimalPad)
+                                TextField("Contributions", text: $contributions)
+                                    .keyboardType(.decimalPad)
+                                TextField("Years", text: $time)
+                                    .keyboardType(.decimalPad)
+                            }
+                            Section {
+                                Text("test")
+                                Text("test1")
+                            }
                         }
-                        List {
-                            Text("test")
-                            Text("test1")
-                        }
-                        Spacer()
                     }
                     .navigationTitle(Text("Title"))
                 }
                 .tabItem {
                     Text("Graph")
                     Image(systemName:"waveform.path.ecg.rectangle")
-                }.tag(2)
+                }.tag(0)
+                VStack {
+                    Form {
+                        Picker("Base", selection: $estBase) {
+                            ForEach(topLine.allCases, id: \.id) { value in
+                                Text(value.localizedName)
+                                    .tag(value)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        TextField("Interest Rate", text: $estInterest)
+                            .keyboardType(.decimalPad)
+                    }
+                }
+                .tabItem {
+                    Text("Rule of 72")
+                    Image(systemName:"waveform.path.ecg.rectangle")
+                }.tag(1)
             }
         }
     }
+}
+
+enum topLine: String, Equatable, CaseIterable, Identifiable {
+    case seventy = "70"
+    case seventyTwo = "72"
+    
+    var localizedName: LocalizedStringKey { LocalizedStringKey(rawValue) }
+    var id: String { self.rawValue }
 }
 
 struct ContentView_Previews: PreviewProvider {
