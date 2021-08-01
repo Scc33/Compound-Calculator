1e117c086c670fee03e00eeca0acf030b226f29b
Sean Coughlin <seanmc4@illinois.edu>
2021-08-01 15:57:15 -0500

    Add personal project

---
+++ PersonalProjects/Compound Solver/Compound Solver/Compound_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compound_SolverApp.swift
+//  Compound Solver
+//
+//  Created by Sean Coughlin on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Compound_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
