47aeb2ca789f0a398596454f55fe5392376ebd3b
Sean Coughlin <seanmc4@illinois.edu>
2021-08-03 20:16:43 -0500

    Add basic code for Compound watch app

---
+++ PersonalProjects/Compound Solver/WatchCompound Extension/Compound_SolverApp.swift
@@ -0,0 +1,19 @@
+//
+//  Compound_SolverApp.swift
+//  WatchCompound Extension
+//
+//  Created by Sean Coughlin on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Compound_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ PersonalProjects/Compound Solver/WatchCompound Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchCompound Extension
+//
+//  Created by Sean Coughlin on 8/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
