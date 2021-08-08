38910bf69533b0e24a0ebbf27f112690da0f3f61
Sean Coughlin <seanmc4@illinois.edu>
2021-08-07 23:14:48 -0500

    Create weather service

---
+++ OtherProjects/YouTube/Weather/Weather/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  Weather
+//
+//  Created by Sean Coughlin on 8/7/21.
+//
+
+import Foundation
---
+++ OtherProjects/YouTube/Weather/Weather/WeatherService.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherService.swift
+//  Weather
+//
+//  Created by Sean Coughlin on 8/7/21.
+//
+
+import Foundation
