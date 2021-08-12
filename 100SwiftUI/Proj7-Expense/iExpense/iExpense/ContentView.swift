447e99b1560ed3f874428bd50ef1647c0d0e7ffc
Sean Coughlin <seanmc4@illinois.edu>
2021-08-11 22:49:43 -0500

    Create @Published to complete other half of puzzle

--- 100SwiftUI/Proj7-Expense/iExpense/iExpense/ContentView.swift
+++ 100SwiftUI/Proj7-Expense/iExpense/iExpense/ContentView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 // Now we will need @ObservedObject instead of @State
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+class User: ObservableObject {
+    //@Published is one half of the puzzle, it sents out a message
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
 }
 
 struct ContentView: View {
+    //@ObservedObject is the other half, it watches for changes
+    @ObservedObject var user = User()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+                    Text("Your name is \(user.firstName) \(user.lastName).")
+
+                    TextField("First name", text: $user.firstName)
+                    TextField("Last name", text: $user.lastName)
+                }
     }
 }
 
