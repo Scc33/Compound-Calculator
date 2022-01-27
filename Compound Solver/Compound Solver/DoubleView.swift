720767d66f549f1e16f2d185fe9c7800246dd6f0
Sean Coughlin <coughlinscc33@gmail.com>
2022-01-26 18:26:45 -0600

    Simplify doubling view

--- Compound Solver/Compound Solver/DoubleView.swift
+++ Compound Solver/Compound Solver/DoubleView.swift
@@ -11,18 +11,18 @@
 
 import SwiftUI
 
-enum topLine: String, Equatable, CaseIterable, Identifiable {
-    case exact = "69.3"
-    case seventy = "70"
-    case seventyTwo = "72"
-    
-    var localizedName: LocalizedStringKey { LocalizedStringKey(rawValue) }
-    var id: String { self.rawValue }
-}
+/*enum topLine: String, Equatable, CaseIterable, Identifiable {
+ case exact = "69.3"
+ case seventy = "70"
+ case seventyTwo = "72"
+ 
+ var localizedName: LocalizedStringKey { LocalizedStringKey(rawValue) }
+ var id: String { self.rawValue }
+ }*/
 
 
 struct DoubleView: View {
-    @State private var estBase: topLine = .seventy
+    //@State private var estBase: topLine = .seventy
     @State private var estInterest = ""
     
     var excDouble: Double {
@@ -32,11 +32,11 @@ struct DoubleView: View {
         return num / dem
     }
     
-    var estDouble: Double {
-        let convertedEstBase = Double(estBase.id) ?? 0.1
-        let convertedEstInterest = Double(estInterest) ?? 0.1
-        return convertedEstBase / convertedEstInterest
-    }
+    /*var estDouble: Double {
+     let convertedEstBase = Double(estBase.id) ?? 0.1
+     let convertedEstInterest = Double(estInterest) ?? 0.1
+     return convertedEstBase / convertedEstInterest
+     }*/
     
     var body: some View {
         NavigationView {
@@ -47,20 +47,24 @@ struct DoubleView: View {
                         TextField("", text: $estInterest)
                             .keyboardType(.decimalPad)
                     }
-                    VStack(alignment: .leading) {
-                        Text("Rule of 72/70/69.3")
-                        Picker("", selection: $estBase) {
-                            ForEach(topLine.allCases, id: \.id) { value in
-                                Text(value.localizedName)
-                                    .tag(value)
-                            }
-                        }
+                    Button("Calculate") {
+                        hideKeyboard()
                     }
-                    .pickerStyle(SegmentedPickerStyle())
+                    /*VStack(alignment: .leading) {
+                     Text("Rule of 72/70/69.3")
+                     Picker("", selection: $estBase) {
+                     ForEach(topLine.allCases, id: \.id) { value in
+                     Text(value.localizedName)
+                     .tag(value)
+                     }.pickerStyle(SegmentedPickerStyle())
+                     }
+                     }*/
                 }
                 Section {
-                    Text("Estimated doubling time \(String(format:"%.2f",estDouble)) years")
-                    Text("Exact doubling time \(String(format:"%.2f",excDouble)) years")
+                    //Text("Estimated doubling time \(String(format:"%.2f",estDouble)) years")
+                    Spacer()
+                    Text("Exact doubling time \(String(format:"%.2f",excDouble)) years").font(.title)
+                    Spacer()
                 }
             }
             .navigationTitle(Text("Doubling Calculator"))
