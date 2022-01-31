399bfb613744c36be4133bc5b72bf9752ed51af6
Sean Coughlin <coughlinscc33@gmail.com>
2022-01-31 12:11:15 -0600

    Calculate doubling time

--- Compound Solver/WatchCompound Extension/ContentView.swift
+++ Compound Solver/WatchCompound Extension/ContentView.swift
@@ -9,17 +9,31 @@ import SwiftUI
 import SwiftUI_Apple_Watch_Decimal_Pad
 
 struct ContentView: View {
-    @State public var presentingModal: Bool = true
-    private var placeholder = "0"
-    @State private var text = "text"
+    @State public var presentingModal: Bool = false
+    private var placeholder = ""
+    @State private var stringInterest = ""
+    
+    func excDouble() -> Double {
+        let interest = Double(stringInterest) ?? 0.0
+        let num = log(2.0)
+        let dem = log(1.0 + (interest / 100.0))
+        return num / dem
+    }
     
     var body: some View {
-        DigiTextView(placeholder: placeholder,
-                     text: $text,
-                     presentingModal: presentingModal,
-                     alignment: .leading,
-                     style: .decimal
-        )
+        VStack {
+            if (stringInterest == "") {
+                Text("Enter an interest rate")
+            } else {
+                Text("Doubling time \(String(format:"%.2f",excDouble())) years")
+            }
+            DigiTextView(placeholder: placeholder,
+                         text: $stringInterest,
+                         presentingModal: presentingModal,
+                         alignment: .leading,
+                         style: .decimal
+            )
+        }
     }
 }
 
